114a15229c9b1918ba363556bc32bc84b61ce578
AlexandrKorytin <al.corytin@yandex.ru>
2024-01-15 15:56:30 +0300

    создан App: Application() и внесен в манифест добавлены зависимости retrofit2, b koin сделана заготовка startKoin в App

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,7 +39,10 @@ android {
 }
 
 dependencies {
-
+    implementation("io.insert-koin:koin-android:3.3.0")
+    implementation("com.google.code.gson:gson:2.10.1")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("androidx.fragment:fragment-ktx:1.6.2")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
