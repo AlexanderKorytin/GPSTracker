edb496b62a59aa89578b5be1c46e1b7fd13888e4
AlexandrKorytin <al.corytin@yandex.ru>
2024-01-15 21:12:06 +0300

    - добавлено проигрывание анимаций во фрагменты Setting, Main, TracksList при создании их View's и при Destroy

--- app/src/main/java/com/example/korytingpstracker/main_menu/ui/fragments/MainFragment.kt
+++ app/src/main/java/com/example/korytingpstracker/main_menu/ui/fragments/MainFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
 import androidx.fragment.app.Fragment
 import com.example.korytingpstracker.databinding.FragmentMainBinding
 
@@ -21,10 +22,20 @@ class MainFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val anim = AnimationUtils.loadAnimation(
+            requireContext(),
+            com.google.android.material.R.anim.abc_fade_in
+        )
+        binding.root.startAnimation(anim)
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
+        val anim = AnimationUtils.loadAnimation(
+            requireContext(),
+            com.google.android.material.R.anim.abc_fade_out
+        )
+        binding.root.startAnimation(anim)
         _binding = null
     }
 
--- app/src/main/java/com/example/korytingpstracker/tracks/ui/fragments/TracksListFragment.kt
+++ app/src/main/java/com/example/korytingpstracker/tracks/ui/fragments/TracksListFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
 import androidx.fragment.app.Fragment
 import com.example.korytingpstracker.databinding.FragmentTracksListBinding
 
@@ -15,17 +16,27 @@ class TracksListFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentTracksListBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val anim = AnimationUtils.loadAnimation(
+            requireContext(),
+            com.google.android.material.R.anim.abc_fade_in
+        )
+        binding.root.startAnimation(anim)
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
+        val anim = AnimationUtils.loadAnimation(
+            requireContext(),
+            com.google.android.material.R.anim.abc_fade_out
+        )
+        binding.root.startAnimation(anim)
         _binding = null
     }
 
