2486d4d8155329d27632df2d6e85763b07063921
AlexandrKorytin <al.corytin@yandex.ru>
2024-01-15 17:56:35 +0300

    небольшой рефакторинг

--- app/src/main/java/com/example/korytingpstracker/tracks/ui/fragments/CurrentTrackFragment.kt
+++ app/src/main/java/com/example/korytingpstracker/tracks/ui/fragments/CurrentTrackFragment.kt
@@ -1,13 +1,11 @@
 package com.example.korytingpstracker.tracks.ui.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.korytingpstracker.R
+import androidx.fragment.app.Fragment
 import com.example.korytingpstracker.databinding.FragmentCurrentTrackBinding
-import com.example.korytingpstracker.databinding.FragmentMainBinding
 
 class CurrentTrackFragment : Fragment() {
     private var _binding: FragmentCurrentTrackBinding? = null
--- app/src/main/java/com/example/korytingpstracker/tracks/ui/fragments/TracksListFragment.kt
+++ app/src/main/java/com/example/korytingpstracker/tracks/ui/fragments/TracksListFragment.kt
@@ -1,12 +1,10 @@
 package com.example.korytingpstracker.tracks.ui.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.korytingpstracker.R
-import com.example.korytingpstracker.databinding.FragmentMainBinding
+import androidx.fragment.app.Fragment
 import com.example.korytingpstracker.databinding.FragmentTracksListBinding
 
 class TracksListFragment : Fragment() {
