3db7d6e5d46585a077e45d1fd7c602b45fd891c9
AlexandrKorytin <al.corytin@yandex.ru>
2024-01-16 14:38:50 +0300

    - ключи для хранения значений SettingsFragment сделаны Enum AppSettingsPrefKeys(пока один) - в SettingsFragment написана логика отображения выбранного значения updatetime

--- app/src/main/java/com/example/korytingpstracker/settings/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/com/example/korytingpstracker/settings/ui/fragments/SettingsFragment.kt
@@ -1,25 +1,21 @@
 package com.example.korytingpstracker.settings.ui.fragments
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.view.animation.AnimationUtils
+import androidx.preference.Preference
+import androidx.preference.Preference.OnPreferenceChangeListener
 import androidx.preference.PreferenceFragmentCompat
 import com.example.korytingpstracker.R
+import com.example.korytingpstracker.settings.ui.models.AppSettingsPrefKeys
 
 class SettingsFragment : PreferenceFragmentCompat() {
+    private val separator = ':'
 
     private var _fragmentView: View? = null
     private val fragmentView get() = _fragmentView!!
+    private lateinit var timePref: Preference
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        return super.onCreateView(inflater, container, savedInstanceState)
-    }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         _fragmentView = view
@@ -27,6 +23,7 @@ class SettingsFragment : PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.app_settigs, rootKey)
+        getSettingsPreference()
     }
 
     override fun onDestroyView() {
@@ -49,5 +46,39 @@ class SettingsFragment : PreferenceFragmentCompat() {
             com.google.android.material.R.anim.abc_fade_out
         )
         view.startAnimation(anim)
+    }
+
+    private fun getSettingsPreference() {
+        timePref = findPreference(AppSettingsPrefKeys.TIMEPREFKEY.value)!!
+        val listener = onPreferenceChangeListner()
+        timePref.onPreferenceChangeListener = listener
+        initPref()
+    }
+
+    private fun onPreferenceChangeListner(): OnPreferenceChangeListener {
+        return OnPreferenceChangeListener { pref, value ->
+            when (pref.key) {
+                AppSettingsPrefKeys.TIMEPREFKEY.value -> {
+                    pref.title = setTitleUpdateTime(pref, value.toString())
+                }
+            }
+            true
+        }
+    }
+
+    private fun initPref() {
+        val updateTimeCurrent = timePref.preferenceManager.sharedPreferences?.getString(
+            AppSettingsPrefKeys.TIMEPREFKEY.value,
+            "3000"
+        )
+        timePref.title = setTitleUpdateTime(timePref, updateTimeCurrent)
+    }
+
+    private fun setTitleUpdateTime(pref: Preference, currentValue: String?): String {
+        val nameArray = resources.getStringArray(R.array.location_time_update_name)
+        val valueArray = resources.getStringArray(R.array.location_time_update_value)
+        return "${
+            pref.title?.split(separator)?.get(0) ?: getString(R.string.update_time)
+        }: ${nameArray[valueArray.indexOf(currentValue)]}"
     }
 }
---
+++ app/src/main/java/com/example/korytingpstracker/settings/ui/models/AppSettingsPrefKeys.kt
@@ -0,0 +1,5 @@
+package com.example.korytingpstracker.settings.ui.models
+
+enum class AppSettingsPrefKeys(val value: String) {
+    TIMEPREFKEY("update_time_key"),
+}
