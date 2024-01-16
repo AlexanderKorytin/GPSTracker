5693582eded1280c3695730b650182a5f2293b03
AlexandrKorytin <al.corytin@yandex.ru>
2024-01-16 18:43:14 +0300

    - до создания View MainFragment вызываем функцию настройки карт у MainViewModel

--- app/src/main/java/com/example/korytingpstracker/main_menu/ui/fragments/MainFragment.kt
+++ app/src/main/java/com/example/korytingpstracker/main_menu/ui/fragments/MainFragment.kt
@@ -7,15 +7,19 @@ import android.view.ViewGroup
 import android.view.animation.AnimationUtils
 import androidx.fragment.app.Fragment
 import com.example.korytingpstracker.databinding.FragmentMainBinding
+import com.example.korytingpstracker.main_menu.ui.viewmodel.MainViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainFragment : Fragment() {
     private var _binding: FragmentMainBinding? = null
     private val binding get() = _binding!!
+    private val mainViewModel by viewModel<MainViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        mainViewModel.configureMap()
         _binding = FragmentMainBinding.inflate(inflater, container, false)
         return binding.root
     }
