941ac65343976b535042443f992196a39df11123
AlexandrKorytin <al.corytin@yandex.ru>
2024-01-16 12:30:18 +0300

    - SettingsFragment сделан наследником PreferenceFragmentCompat() для избавления от написания кода сохранения настроек - переделан запуск анимации SettingsFragment -

--- app/src/main/java/com/example/korytingpstracker/settings/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/com/example/korytingpstracker/settings/ui/fragments/SettingsFragment.kt
@@ -5,46 +5,49 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.animation.AnimationUtils
-import androidx.fragment.app.Fragment
-import com.example.korytingpstracker.databinding.FragmentSettingsBinding
+import androidx.preference.PreferenceFragmentCompat
+import com.example.korytingpstracker.R
 
-class SettingsFragment : Fragment() {
-    private var _binding: FragmentSettingsBinding? = null
-    private val binding get() = _binding!!
+class SettingsFragment : PreferenceFragmentCompat() {
+
+    private var _fragmentView: View? = null
+    private val fragmentView get() = _fragmentView!!
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        _binding = FragmentSettingsBinding.inflate(inflater, container, false)
-        return binding.root
+    ): View {
+        return super.onCreateView(inflater, container, savedInstanceState)
     }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val anim = AnimationUtils.loadAnimation(
-            requireContext(),
-            com.google.android.material.R.anim.abc_fade_in
-        )
-        binding.root.startAnimation(anim)
+        _fragmentView = view
+    }
+
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        setPreferencesFromResource(R.xml.app_settigs, rootKey)
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
+        playExitAnim(fragmentView)
+        _fragmentView = null
+    }
+
+    private fun playExitAnim(view: View) {
+        val anim = AnimationUtils.loadAnimation(
+            requireContext(),
+            com.google.android.material.R.anim.abc_fade_in
+        )
+        view.startAnimation(anim)
+    }
+
+    private fun playEnterAnim(view: View) {
         val anim = AnimationUtils.loadAnimation(
             requireContext(),
             com.google.android.material.R.anim.abc_fade_out
         )
-        binding.root.startAnimation(anim)
-        _binding = null
-    }
-
-    companion object {
-        fun newInstance(param1: String, param2: String) =
-            SettingsFragment().apply {
-                arguments = Bundle().apply {
-
-                }
-            }
+        view.startAnimation(anim)
     }
 }
