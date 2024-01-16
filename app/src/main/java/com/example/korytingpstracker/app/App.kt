33a292f6dbb6cb89ba7392e76051c0c713b55d52
AlexandrKorytin <al.corytin@yandex.ru>
2024-01-16 20:02:37 +0300

    - проинициализировал PermissionRequeste

--- app/src/main/java/com/example/korytingpstracker/app/App.kt
+++ app/src/main/java/com/example/korytingpstracker/app/App.kt
@@ -4,12 +4,15 @@ import android.app.Application
 import com.example.korytingpstracker.main_menu.di.mainDataModule
 import com.example.korytingpstracker.main_menu.di.mainDomainModule
 import com.example.korytingpstracker.main_menu.di.mainVMModule
+import com.markodevcic.peko.PermissionRequester
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
+
+        PermissionRequester.initialize(applicationContext)
 
         startKoin {
             androidContext(applicationContext)
