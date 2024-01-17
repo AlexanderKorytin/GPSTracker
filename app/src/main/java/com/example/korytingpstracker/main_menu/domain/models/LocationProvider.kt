9575d74ee743dd99f62b1dbcdd172a1b2b72e8dc
AlexandrKorytin <al.corytin@yandex.ru>
2024-01-17 13:01:03 +0300

    - создан дата класс

---
+++ app/src/main/java/com/example/korytingpstracker/main_menu/domain/models/LocationProvider.kt
@@ -0,0 +1,6 @@
+package com.example.korytingpstracker.main_menu.domain.models
+
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider
+
+data class LocationProvider(val provider: GpsMyLocationProvider) {
+}
