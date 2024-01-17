c331fcd88b75882f93d98d71a290958406cad78f
AlexandrKorytin <al.corytin@yandex.ru>
2024-01-17 13:06:00 +0300

    - в MainViewModel в конструктор добавлен SettingsInteractor, написана функция сохранения флага показа диалонга

--- app/src/main/java/com/example/korytingpstracker/main_menu/ui/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/korytingpstracker/main_menu/ui/viewmodel/MainViewModel.kt
@@ -4,17 +4,25 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.korytingpstracker.main_menu.domain.api.MainInteractor
+import com.example.korytingpstracker.settings.ui.domain.api.SettingsInteractor
 import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider
 
-class MainViewModel(private val interactor: MainInteractor) : ViewModel() {
+class MainViewModel(
+    private val mainInteractor: MainInteractor,
+    private val settingsInteractor: SettingsInteractor
+) : ViewModel() {
     private var locationProvider: MutableLiveData<GpsMyLocationProvider> = MutableLiveData(null)
 
     fun getLocationProviderValue(): LiveData<GpsMyLocationProvider> = locationProvider
     fun configureMap() {
-        interactor.configureMap()
+        mainInteractor.configureMap()
+    }
+
+    fun saveIsNeedShowDialog(isNeed: Boolean) {
+        settingsInteractor.saveIsNeedDialogShow(isNeed)
     }
 
     fun getProvider() {
-        locationProvider.postValue(interactor.getLocationProvider().provider)
+        locationProvider.postValue(mainInteractor.getLocationProvider().provider)
     }
 }
