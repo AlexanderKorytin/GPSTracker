31324eb5f085954accf66bbaf230d53b610ea8e2
AlexandrKorytin <al.corytin@yandex.ru>
2024-01-17 13:00:14 +0300

    - в App добавлена переменная показа сообщения перед переходом в настройки при получении разрешения BackGround Location, добавлена логика получения ее из хранилища

--- app/src/main/java/com/example/korytingpstracker/app/App.kt
+++ app/src/main/java/com/example/korytingpstracker/app/App.kt
@@ -1,27 +1,45 @@
 package com.example.korytingpstracker.app
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatActivity
 import com.example.korytingpstracker.main_menu.di.mainDataModule
 import com.example.korytingpstracker.main_menu.di.mainDomainModule
 import com.example.korytingpstracker.main_menu.di.mainVMModule
+import com.example.korytingpstracker.settings.ui.di.settingsDataModule
+import com.example.korytingpstracker.settings.ui.di.settingsDomainModule
 import com.markodevcic.peko.PermissionRequester
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
+const val APP_SETTINGS_PREF_KEY = "App settings"
+const val IS_NEED_SHOW_DIALOG = "is need show dialog"
+
 class App : Application() {
+    companion object {
+        var needDialogShow = true
+    }
+
     override fun onCreate() {
         super.onCreate()
 
         PermissionRequester.initialize(applicationContext)
+
+        needDialogShow =
+            getSharedPreferences(APP_SETTINGS_PREF_KEY, AppCompatActivity.MODE_PRIVATE)
+                .getBoolean(IS_NEED_SHOW_DIALOG, true)
 
         startKoin {
             androidContext(applicationContext)
             modules(
                 mainDataModule,
                 mainDomainModule,
-                mainVMModule
+                mainVMModule,
+
+                settingsDataModule,
+                settingsDomainModule
             )
         }
 
     }
+
 }
