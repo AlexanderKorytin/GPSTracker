c9508b4f09ff6a41a6952d2bf0395f1906c450fa
AlexandrKorytin <al.corytin@yandex.ru>
2024-02-18 09:33:58 +0300

    - рефакторинг

--- app/src/main/java/com/example/korytingpstracker/main_menu/data/MainRepositoryImpl.kt
+++ app/src/main/java/com/example/korytingpstracker/main_menu/data/MainRepositoryImpl.kt
@@ -6,7 +6,8 @@ import com.example.korytingpstracker.main_menu.domain.api.MainRepository
 import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider
 
 class MainRepositoryImpl(
-    private val mapClient: MapClient) : MainRepository {
+    private val mapClient: MapClient
+) : MainRepository {
     override fun configureMap() {
         mapClient.configureMap()
     }
