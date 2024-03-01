51c8c10cb0c694f1e5c57f718b52744f7bc43935
AlexandrKorytin <al.corytin@yandex.ru>
2024-03-01 21:55:07 +0300

    - форматирование

--- app/src/main/java/com/example/korytingpstracker/main_menu/di/MainDataModule.kt
+++ app/src/main/java/com/example/korytingpstracker/main_menu/di/MainDataModule.kt
@@ -1,19 +1,13 @@
 package com.example.korytingpstracker.main_menu.di
 
-import android.app.Notification
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.app.PendingIntent
-import android.content.Intent
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.NotificationCompat
-import com.example.korytingpstracker.R
 import com.example.korytingpstracker.app.APP_SETTINGS_PREF_KEY
-import com.example.korytingpstracker.core.ui.MainActivity
 import com.example.korytingpstracker.main_menu.data.api.MapClient
 import com.example.korytingpstracker.main_menu.data.network.OpenStreetMapClient
 import com.example.korytingpstracker.main_menu.data.service.LocationService
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
