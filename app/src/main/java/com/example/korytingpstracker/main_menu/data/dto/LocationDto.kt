57fbbcebb01eac00810b1f2314a1f5a08525f798
AlexandrKorytin <al.corytin@yandex.ru>
2024-03-01 21:47:54 +0300

    - сделал класс Serializable

--- app/src/main/java/com/example/korytingpstracker/main_menu/data/dto/LocationDto.kt
+++ app/src/main/java/com/example/korytingpstracker/main_menu/data/dto/LocationDto.kt
@@ -1,9 +1,10 @@
 package com.example.korytingpstracker.main_menu.data.dto
 
 import org.osmdroid.util.GeoPoint
+import java.io.Serializable
 
 data class LocationDto(
     val speed: Float = 0f,
     val distance: Float = 0f,
     val geoPointList: ArrayList<GeoPoint>,
-)
+): Serializable
