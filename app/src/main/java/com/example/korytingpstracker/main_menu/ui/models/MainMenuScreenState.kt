4dd70ffd798822edfe6be92edb45fefcaea61beb
AlexandrKorytin <al.corytin@yandex.ru>
2024-03-08 23:44:43 +0300

    - форматирование

--- app/src/main/java/com/example/korytingpstracker/main_menu/ui/models/MainMenuScreenState.kt
+++ app/src/main/java/com/example/korytingpstracker/main_menu/ui/models/MainMenuScreenState.kt
@@ -1,15 +1,5 @@
 package com.example.korytingpstracker.main_menu.ui.models
 
-import org.osmdroid.util.GeoPoint
-
 sealed interface MainMenuScreenState {
-    data class Content(
-        val speed: String = SPEED_START,
-        val distance: String = DISTANCE_START,
-        val averageSpeed: String = SPEED_START,
-        val geoPointList: List<GeoPoint> = emptyList()
-    ): MainMenuScreenState
+    data class Content(val data: LocationTrack) : MainMenuScreenState
 }
-
-private const val SPEED_START = "0.0"
-private const val DISTANCE_START = "0"
