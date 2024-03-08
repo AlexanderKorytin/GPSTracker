c3c934c6a0f0cbddfb00510480f86a4deaaaddd4
AlexandrKorytin <al.corytin@yandex.ru>
2024-03-08 23:40:26 +0300

    - создан дао интерфейс

---
+++ app/src/main/java/com/example/korytingpstracker/tracks/data/dao/LocationsTracksDao.kt
@@ -0,0 +1,23 @@
+package com.example.korytingpstracker.tracks.data.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.korytingpstracker.tracks.data.entity.LocTrackEntity
+
+@Dao
+interface LocationsTracksDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun saveLocationTrack(locTrack: LocTrackEntity)
+
+    @Query("SELECT * FROM Locations_tracks")
+    suspend fun getAllLocTracks(): List<LocTrackEntity>
+
+    @Query("SELECT * FROM Locations_tracks WHERE trackName = :trackName")
+    suspend fun getLocTrackByName(trackName: String): LocTrackEntity
+
+    @Delete(LocTrackEntity::class)
+    suspend fun deleteLocTrack(locTrack: LocTrackEntity)
+}
