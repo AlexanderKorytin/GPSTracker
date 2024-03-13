665f4765116e1c57a9967d98be085a07ffc47924
AlexandrKorytin <al.corytin@yandex.ru>
2024-03-13 21:16:30 +0300

    - форматирование

--- app/src/main/java/com/example/korytingpstracker/tracks/data/repository/LocTrackRepositoryImpl.kt
+++ app/src/main/java/com/example/korytingpstracker/tracks/data/repository/LocTrackRepositoryImpl.kt
@@ -2,7 +2,7 @@ package com.example.korytingpstracker.tracks.data.repository
 
 import com.example.korytingpstracker.core.ui.db.AppDataBase
 import com.example.korytingpstracker.main_menu.ui.models.LocationTrack
-import com.example.korytingpstracker.tracks.data.convertor.map
+import com.example.korytingpstracker.tracks.data.db.convertor.map
 import com.example.korytingpstracker.tracks.domain.api.LocationTrackRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
