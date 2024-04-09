fb4f2658bd0dbf6ff80acdf2e7c186de8413c8e0
AlexandrKorytin <al.corytin@yandex.ru>
2024-04-09 20:18:35 +0300

    рефакторинг

--- app/src/main/java/com/example/korytingpstracker/tracks/ui/LocationTrackAdapter.kt
+++ app/src/main/java/com/example/korytingpstracker/tracks/ui/LocationTrackAdapter.kt
@@ -20,8 +20,5 @@ class LocationTrackAdapter(
 
     override fun onBindViewHolder(holder: LocationTrackViewHolder, position: Int) {
         holder.bind(currentList[position])
-        holder.itemView.setOnClickListener {
-            onItemClick(currentList[position])
-        }
     }
 }
