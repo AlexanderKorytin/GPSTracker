ee8da37711f76d7788fed4308c23046ad15b90dc
AlexandrKorytin <al.corytin@yandex.ru>
2024-04-19 19:23:42 +0300

    добавлена функция настройки карты

--- app/src/main/java/com/example/korytingpstracker/tracks/ui/viewmodel/LocationTrackViewModel.kt
+++ app/src/main/java/com/example/korytingpstracker/tracks/ui/viewmodel/LocationTrackViewModel.kt
@@ -1,9 +1,9 @@
 package com.example.korytingpstracker.tracks.ui.viewmodel
 
-import androidx.annotation.StringRes
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.korytingpstracker.main_menu.domain.api.MainInteractor
 import com.example.korytingpstracker.main_menu.ui.models.LocationTrack
 import com.example.korytingpstracker.main_menu.ui.models.MainMenuScreenState
 import com.example.korytingpstracker.settings.ui.domain.api.SettingsInteractor
@@ -12,14 +12,16 @@ import com.example.korytingpstracker.tracks.ui.models.LocationTracksScreenState
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
-class LocationTrackViewModel(private val dataBaseInteractor: LocationTrackInteractor,
-                             private val settingsInteractor: SettingsInteractor,
-    ) : ViewModel() {
+class LocationTrackViewModel(
+    private val dataBaseInteractor: LocationTrackInteractor,
+    private val settingsInteractor: SettingsInteractor,
+    private val mapInteractor: MainInteractor
+) : ViewModel() {
     private var _listTracksScreenState = MutableLiveData<LocationTracksScreenState>()
     val listTracksScreenState get() = _listTracksScreenState
 
     private var _currentTrackScreenState = MutableLiveData<MainMenuScreenState>()
-    val currentTrackScreenState get() =  _currentTrackScreenState
+    val currentTrackScreenState get() = _currentTrackScreenState
 
     fun getAllLocTracks() {
         viewModelScope.launch(Dispatchers.IO) {
@@ -40,12 +42,16 @@ class LocationTrackViewModel(private val dataBaseInteractor: LocationTrackIntera
         }
     }
 
-    fun getTrackById(trackId: Long){
-        viewModelScope.launch(Dispatchers.IO){
-            dataBaseInteractor.getLocTrackById(trackId).collect{
+    fun getTrackById(trackId: Long) {
+        viewModelScope.launch(Dispatchers.IO) {
+            dataBaseInteractor.getLocTrackById(trackId).collect {
                 _currentTrackScreenState.postValue(MainMenuScreenState.Content(it))
             }
         }
+    }
+
+    fun configureMap(){
+        mapInteractor.configureMap()
     }
 
     fun getColorLine(): Int {
